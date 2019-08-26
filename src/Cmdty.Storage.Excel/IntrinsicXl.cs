ea6a71ffb3a354dc37d7304f0d7b36f26e0d08be
Jake Fowler <fowlerjake@hotmail.com>
2019-08-26 08:32:21 +0100

    Futher work on Excel add-in.

---
+++ src/Cmdty.Storage.Excel/IntrinsicXl.cs
@@ -0,0 +1,117 @@
+﻿#region License
+// Copyright (c) 2019 Jake Fowler
+//
+// Permission is hereby granted, free of charge, to any person 
+// obtaining a copy of this software and associated documentation 
+// files (the "Software"), to deal in the Software without 
+// restriction, including without limitation the rights to use, 
+// copy, modify, merge, publish, distribute, sublicense, and/or sell 
+// copies of the Software, and to permit persons to whom the 
+// Software is furnished to do so, subject to the following 
+// conditions:
+//
+// The above copyright notice and this permission notice shall be 
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+// OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+
+using System;
+using Cmdty.Storage.Core;
+using Cmdty.TimePeriodValueTypes;
+using Cmdty.TimeSeries;
+using ExcelDna.Integration;
+
+namespace Cmdty.Storage.Excel
+{
+    public static class IntrinsicXl
+    {
+        [ExcelFunction(Name = AddIn.ExcelFunctionNamePrefix + nameof(StorageIntrinsicValue), Category = AddIn.ExcelFunctionCategory, 
+            IsThreadSafe = true, IsVolatile = false, IsExceptionSafe = true)]
+        public static object StorageIntrinsicValue(
+            DateTime valuationDate,
+            DateTime storageStart,
+            DateTime storageEnd,
+            object injectWithdrawConstraints,
+            double injectionCostRate,
+            double cmdtyConsumedOnInjection,
+            double withdrawalCostRate,
+            double cmdtyConsumedOnWithdrawal,
+            double currentInventory,
+            object forwardCurve,
+            object interestRateCurve,
+            double gridSpacing,
+            [ExcelArgument(Name = "Granularity")] object granularity
+            )
+        {
+
+            return StorageNpv<Day>(valuationDate, storageStart, storageEnd, injectWithdrawConstraints, 
+                injectionCostRate, cmdtyConsumedOnInjection, withdrawalCostRate, 
+                cmdtyConsumedOnWithdrawal,
+                currentInventory, forwardCurve, interestRateCurve, gridSpacing);
+        }
+
+        private static double StorageNpv<T>(DateTime valuationDateTime,
+            DateTime storageStartDateTime,
+            DateTime storageEndDateTime,
+            object injectWithdrawConstraints,
+            double injectionCostRate,
+            double cmdtyConsumedOnInjection,
+            double withdrawalCostRate,
+            double cmdtyConsumedOnWithdrawal,
+            double currentInventory,
+            object forwardCurveIn,
+            object interestRateCurve,
+            double gridSpacing)
+            where T : ITimePeriod<T>
+        {
+            T currentPeriod = TimePeriodFactory.FromDateTime<T>(valuationDateTime);
+
+            DoubleTimeSeries<T> forwardCurve = StorageExcelHelper.CreateDoubleTimeSeries<T>(forwardCurveIn, "Forward_curve");
+            
+            T storageStart = TimePeriodFactory.FromDateTime<T>(storageStartDateTime);
+            T storageEnd = TimePeriodFactory.FromDateTime<T>(storageEndDateTime);
+
+            TimeSeries<Month, Day> settlementDates = new TimeSeries<Month, Day>.Builder
+            {
+                {new Month(2019, 9),  new Day(2019, 10, 5)}
+            }.Build();
+
+            CmdtyStorage<T> storage = CmdtyStorage<T>.Builder
+                .WithActiveTimePeriod(storageStart, storageEnd)
+                .WithConstantInjectWithdrawRange(-45.5, 56.6)
+                .WithConstantMinInventory(0.0)
+                .WithConstantMaxInventory(1000.0)
+                .WithPerUnitInjectionCost(0.8, injectionDate => injectionDate.First<Day>())
+                .WithNoCmdtyConsumedOnInject()
+                .WithPerUnitWithdrawalCost(1.2, withdrawalDate => withdrawalDate.First<Day>())
+                .WithNoCmdtyConsumedOnWithdraw()
+                .MustBeEmptyAtEnd()
+                .Build();
+
+            IntrinsicStorageValuationResults<T> valuationResults = IntrinsicStorageValuation<T>
+                .ForStorage(storage)
+                .WithStartingInventory(currentInventory)
+                .ForCurrentPeriod(currentPeriod)
+                .WithForwardCurve(forwardCurve)
+                .WithMonthlySettlement(settlementDates)
+                .WithDiscountFactorFunc(day => 1.0)
+                .WithGridSpacing(gridSpacing)
+                .Calculate();
+
+            return valuationResults.NetPresentValue;
+        }
+
+
+
+ 
+        
+    }
+}
--- src/Cmdty.Storage.Excel/StorageExcelHelper.cs
+++ src/Cmdty.Storage.Excel/StorageExcelHelper.cs
@@ -24,6 +24,10 @@
 #endregion
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Cmdty.Storage.Core;
 using Cmdty.TimePeriodValueTypes;
 using Cmdty.TimeSeries;
@@ -31,70 +35,74 @@ using ExcelDna.Integration;
 
 namespace Cmdty.Storage.Excel
 {
-    public static class StorageXl
+    public static class StorageExcelHelper
     {
-        [ExcelFunction(Name = AddIn.ExcelFunctionNamePrefix + nameof(StorageIntrinsicValue), Category = AddIn.ExcelFunctionCategory, 
-            IsThreadSafe = true, IsVolatile = false, IsExceptionSafe = true)]
-        public static object StorageIntrinsicValue(
-            DateTime valuationDate,
-            DateTime storageStart,
-            DateTime storageEnd,
-            double currentInventory,
-            object forwardCurve,
-            double gridSpacing,
-            [ExcelArgument(Name = "Granularity")] object granularity
-            )
-        {
 
-            return StorageNpv<Day>(valuationDate, storageStart, storageEnd, currentInventory, forwardCurve, gridSpacing);
-        }
-
-        private static double StorageNpv<T>(DateTime valuationDateTime,
-            DateTime storageStartDateTime,
+        private static CmdtyStorage<T> CreateCmdtyStorageFromExcelInputs<T>(DateTime storageStartDateTime,
             DateTime storageEndDateTime,
-            double currentInventory,
-            object forwardCurveIn, double gridSpacing)
+            object injectWithdrawConstraintsIn,
+            double injectionCostRate,
+            double cmdtyConsumedOnInjection,
+            double withdrawalCostRate,
+            double cmdtyConsumedOnWithdrawal)
             where T : ITimePeriod<T>
         {
-            T currentPeriod = TimePeriodFactory.FromDateTime<T>(valuationDateTime);
-
-            DoubleTimeSeries<T> forwardCurve = CreateDoubleTimeSeries<T>(forwardCurveIn, "Forward_curve");
-            
             T storageStart = TimePeriodFactory.FromDateTime<T>(storageStartDateTime);
             T storageEnd = TimePeriodFactory.FromDateTime<T>(storageEndDateTime);
 
-            TimeSeries<Month, Day> settlementDates = new TimeSeries<Month, Day>.Builder
+            if (injectWithdrawConstraintsIn is ExcelMissing || injectWithdrawConstraintsIn is ExcelEmpty)
+                throw new ArgumentException("Inject/withdraw constraints haven't been specified.");
+
+            if (!(injectWithdrawConstraintsIn is object[,] injectWithdrawArray))
+                throw new ArgumentException("Inject/withdraw constraints have been incorrectly entered. Argument value should be of a range with 4 columns, the first containing dates, the rest containing numbers.");
+
+            if (injectWithdrawArray.GetLength(1) != 4)
+                throw new ArgumentException("Inject/withdraw constraints have been incorrectly entered. Argument value should be a range 4 columns.");
+
+
+            var injectWith = TakeWhileNotEmpty(injectWithdrawArray).Select((row, i) => new
             {
-                {new Month(2019, 9),  new Day(2019, 10, 5)}
-            }.Build();
+                period = row[0],
+                inventory = ObjectToDouble(row[1], $"Row {i + 1} of inject/withdraw/inventory constraints contains invalid inventory in 2nd column as is not a number."),
+                injectRate = ObjectToDouble(row[2], $"Row {i + 1} of inject/withdraw/inventory constraints contains invalid injection rate in 3rd column as is not a number."),
+                withdrawRate = ObjectToDouble(row[3], $"Row {i + 1} of inject/withdraw/inventory constraints contains invalid withdrawal in 4th column as is not a number.")
+            }).GroupBy(arg => arg.period);
+
 
             CmdtyStorage<T> storage = CmdtyStorage<T>.Builder
-                .WithActiveTimePeriod(storageStart, storageEnd)
-                .WithConstantInjectWithdrawRange(-45.5, 56.6)
-                .WithConstantMinInventory(0.0)
-                .WithConstantMaxInventory(1000.0)
-                .WithPerUnitInjectionCost(0.8, injectionDate => injectionDate.First<Day>())
-                .WithNoCmdtyConsumedOnInject()
-                .WithPerUnitWithdrawalCost(1.2, withdrawalDate => withdrawalDate.First<Day>())
-                .WithNoCmdtyConsumedOnWithdraw()
-                .MustBeEmptyAtEnd()
-                .Build();
+            .WithActiveTimePeriod(storageStart, storageEnd)
+            .WithConstantInjectWithdrawRange(-45.5, 56.6)
+            .WithConstantMinInventory(0.0)
+            .WithConstantMaxInventory(1000.0)
+            .WithPerUnitInjectionCost(injectionCostRate, injectionDate => injectionDate.First<Day>())
+            .WithFixedPercentCmdtyConsumedOnInject(cmdtyConsumedOnInjection)
+            .WithPerUnitWithdrawalCost(withdrawalCostRate, withdrawalDate => withdrawalDate.First<Day>())
+            .WithFixedPercentCmdtyConsumedOnWithdraw(cmdtyConsumedOnWithdrawal)
+            .MustBeEmptyAtEnd()
+            .Build();
 
-            IntrinsicStorageValuationResults<T> valuationResults = IntrinsicStorageValuation<T>
-                .ForStorage(storage)
-                .WithStartingInventory(currentInventory)
-                .ForCurrentPeriod(currentPeriod)
-                .WithForwardCurve(forwardCurve)
-                .WithMonthlySettlement(settlementDates)
-                .WithDiscountFactorFunc(day => 1.0)
-                .WithGridSpacing(gridSpacing)
-                .Calculate();
-
-            return valuationResults.NetPresentValue;
+            return storage;
         }
 
-        // TODO put this function in common location to be reused
-        private static DoubleTimeSeries<T> CreateDoubleTimeSeries<T>(object excelValues, string excelArgumentName)
+        private static IEnumerable<object[]> TakeWhileNotEmpty(object[,] excelInput)
+        {
+            int numColumns = excelInput.GetLength(1);
+            for (int i = 0; i < excelInput.GetLength(0); i++)
+            {
+                if (excelInput[i, 0] is ExcelEmpty)
+                    yield break;
+
+                var slice = new object[numColumns];
+                for (int j = 0; j < numColumns; j++)
+                {
+                    slice[j] = excelInput[i, j];
+                }
+
+                yield return slice;
+            }
+        }
+
+        public static DoubleTimeSeries<T> CreateDoubleTimeSeries<T>(object excelValues, string excelArgumentName)
             where T : ITimePeriod<T>
         {
             if (excelValues is ExcelMissing || excelValues is ExcelEmpty)
@@ -102,7 +110,7 @@ namespace Cmdty.Storage.Excel
 
             if (!(excelValues is object[,] excelValuesArray))
                 throw new ArgumentException(excelArgumentName + " has been incorrectly entered. Argument value should be of a range with 2 columns, the first containing dates, the second containing numbers.");
-            
+
             if (excelValuesArray.GetLength(1) != 2)
                 throw new ArgumentException(excelArgumentName + " has been incorrectly entered. Argument value should be a range 2 columns.");
 
@@ -112,7 +120,7 @@ namespace Cmdty.Storage.Excel
             {
                 if (excelValuesArray[i, 0] is ExcelEmpty)
                     break;
-                
+
                 if (!(excelValuesArray[i, 1] is double doubleValue))
                     throw new ArgumentException($"Value in the second column of row {i} for argument {excelArgumentName} is not a number.");
 
@@ -121,15 +129,22 @@ namespace Cmdty.Storage.Excel
 
                 builder.Add(curvePointPeriod, doubleValue);
             }
-            
+
             return builder.Build();
+        }
+
+        private static double ObjectToDouble(object excelNumber, string messageOneFail)
+        {
+            if (!(excelNumber is double doubleNumber))
+                throw new ArgumentException(messageOneFail);
+            return doubleNumber;
         }
 
         private static DateTime ObjectToDateTime(object excelDateTime, string excelArgumentName)
         {
             if (!(excelDateTime is double doubleValue))
                 throw new ArgumentException();
-            
+
             try
             {
                 DateTime dateTime = DateTime.FromOADate(doubleValue);
@@ -141,6 +156,6 @@ namespace Cmdty.Storage.Excel
             }
 
         }
-        
+
     }
 }
